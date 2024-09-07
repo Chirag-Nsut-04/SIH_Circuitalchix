895edb56ce10c197db581ddad08f5cb735d9b57f
divyamzzz <divyam.singh.ug21@nsut.ac.in>
2024-09-08 01:24:29 +0530

    new repo

---
+++ src/chatbot.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ChatBot = () => {
+  return (
+    <div>
+      <iframe
+        title="Dialogflow Chatbot"
+        allow="microphone;"
+        width="350"
+        height="430"
+        src="https://console.dialogflow.com/api-client/demo/embedded/81358a5f-d935-45f3-aeb4-ccb8e5f1d93e"
+      ></iframe>
+    </div>
+  );
+};
+
+export default ChatBot;
